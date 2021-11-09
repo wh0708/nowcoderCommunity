d776d1fe7c8d65d1fb3f7eb5c02af11875e581d3
wh <296006803@qq.com>
2021-11-09 13:36:18 +0800

    注册功能

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,26 @@
+package com.nowcoder.community.util;
+
+/**
+ * @Author WH
+ * @create 2021/11/8 21:56
+ */
+
+
+public interface CommunityConstant {
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS=0;
+
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT=1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE=2;
+
+}
+
