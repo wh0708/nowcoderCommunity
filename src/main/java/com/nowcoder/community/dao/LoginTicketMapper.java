b6fce9fa697f7e9a4ce87ab6499450df69ea17e7
wh <296006803@qq.com>
2021-11-10 16:31:45 +0800

    登录、退出功能

---
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * @Author WH
+ * @create 2021/11/10 14:22
+ */
+@Mapper
+public interface LoginTicketMapper {
+    @Insert({
+            "insert into login_ticket(user_id,ticket,status,expired) ",
+            "values(#{userId},#{ticket},#{status},#{expired})"
+    })
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    int insertLoginTicket(LoginTicket ticket);
+
+    @Select({
+            "select id,user_id,ticket,status,expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+//    动态sql
+    @Update({
+            "<script>",
+            "update login_ticket set status=#{status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\" >",
+            "and 1=1",
+            "</if>",
+            "</script>"
+    })
+    int updateStatus(String ticket,int status);
+
+}
