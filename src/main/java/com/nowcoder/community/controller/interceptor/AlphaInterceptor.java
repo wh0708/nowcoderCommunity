f3152e864b8e0cf6a516302919994a4a2558413a
wh <296006803@qq.com>
2021-11-13 15:15:27 +0800

    上传头像&修改密码

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,40 @@
+package com.nowcoder.community.controller.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author WH
+ * @create 2021/11/11 16:50
+ */
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+    private static final Logger logger= LoggerFactory.getLogger(AlphaInterceptor.class);
+
+    //在Controller之前执行 handler是拦截的目标方法
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("preHandle: "+handler.toString());
+        return true;
+    }
+
+    //在Controller之后执行,TemplateEngine之前执行
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        logger.debug("postHandle: "+handler.toString());
+
+    }
+
+    //在TemplateEngine之后执行
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        logger.debug("afterCompletion: "+handler.toString());
+    }
+}
