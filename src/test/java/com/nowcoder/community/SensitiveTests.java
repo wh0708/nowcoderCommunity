464456d285dc90caf9c0dcc66d88509ab10f20ed
wh <296006803@qq.com>
2021-11-15 16:03:40 +0800

    敏感词过滤

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,29 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author WH
+ * @create 2021/11/15 15:50
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter(){
+        String text="反@对@嫖@娼，吸@毒@，赌@博，开@票";
+        text=sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
