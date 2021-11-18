bdbd9aeb777d9734d8fc5890aae4119feda65ef8
wh <296006803@qq.com>
2021-11-18 13:33:29 +0800

    帖子详情

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,12 +1,25 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 /**
  * @Author WH
@@ -18,6 +31,15 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
+    @Autowired(required = false)
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService(){
         System.out.println("实例化AlphaService");
@@ -36,4 +58,65 @@ public class AlphaService {
     public String find(){
         return alphaDao.select();
     }
+
+    //propagation传播机制 事务交叉时
+    //REQUIRED(0) 支持当前事务（外部事务），如果不存在则创建新事务
+    //REQUIRES_NEW(3) 创建一个新的事务，并且暂停当前事务（外部事务）
+    //NESTED(6) 如果当前存在事务（外部事务），则嵌套在该事务中执行（独立的提交和回滚），否则就和REQUIRED一样
+    @Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED)
+    public Object save1(){
+        //新增用户
+        User user=new User();
+        user.setUsername("test");
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+        //新增帖子
+        DiscussPost post=new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("hello");
+        post.setContent("新人报道");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    public Object save2(){
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+
+
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                //新增用户
+                User user=new User();
+                user.setUsername("testA");
+                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+                user.setEmail("testA@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+                //新增帖子
+                DiscussPost post=new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("helloA");
+                post.setContent("新人报道A");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                Integer.valueOf("abc");
+                return "ok";
+            }
+        });
+    }
+
 }
