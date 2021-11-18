d2fd00096c95bed1057d88b67a2e877a651b2b04
wh <296006803@qq.com>
2021-11-18 20:31:27 +0800

    显示评论

---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author WH
+ * @create 2021/11/18 13:35
+ */
+@Mapper
+@Repository
+public interface CommentMapper {
+
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType,int entityId);
+}
