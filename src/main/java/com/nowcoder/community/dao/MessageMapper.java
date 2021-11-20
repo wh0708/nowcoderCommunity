f0b32fe85157e939a25de32817f7f9c602a72681
wh <296006803@qq.com>
2021-11-20 15:20:40 +0800

    发送私信

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -29,6 +29,11 @@ public interface MessageMapper {
     //查询未读私信的数量
     int selectLetterUnreadCount(int userId,String conversationId);
 
+    //新增消息
+    int insertMessage(Message message);
+
+    //修改状态
+    int updateStatus(List<Integer> ids,int status);
 
 }
 
